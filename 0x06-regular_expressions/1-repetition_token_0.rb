e59cfdb3eaf28524676ea2d165cb1f589005637f
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-31 01:58:03 +0200

    1 0x06

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# Define the regular expression pattern
+pattern = /hbttt*n/
+
+# Get the input string from the command line arguments
+input_string = ARGV[0]
+
+# Perform the matching operation
+matches = input_string.scan(pattern)
+
+# Output the matches
+puts matches.join('')

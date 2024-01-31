22752f692d26f2851f07976622321cb1bf1f3b87
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-31 02:00:50 +0200

    1 0x06

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Define the regular expression pattern
-pattern = /hbttt*n/
+pattern = /hbt{2,5}n/
 
 # Get the input string from the command line arguments
 input_string = ARGV[0]

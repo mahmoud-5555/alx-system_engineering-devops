dfcc9fb3d3d04b2772aea4debe24675f63c440f5
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-23 04:06:10 +0200

    1 0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',  # Ensure Flask is installed with version 2.1.0
   provider => 'pip3',   # Use pip3 as the provider
 }
+package {'werkzeurg':
+  ensure   => '2.1.1',  # Ensure Flask is installed with version 2.1.0
+  provider => 'pip3',   # Use pip3 as the provider
+
+}

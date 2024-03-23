bb66e15b484abb49b6d26e0230ee76da47c28ae4
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-23 04:29:00 +0200

    2 0x0A

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Define an exec resource to kill the process named "killmenow"
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',  # Command to kill the process
-  path        => ['/bin', '/usr/bin'],  # Set path to locate the pkill command
-  refreshonly => true,                  # Only run when notified
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,                  
 }

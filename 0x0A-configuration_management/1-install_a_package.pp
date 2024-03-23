ad1eabc6e8140ac17442ff237d355b28e8f3e680
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-23 03:59:58 +0200

    1 0x0A

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Define a package resource to ensure Flask is installed via pip3
+package { 'flask':
+  ensure   => '2.1.0',  # Ensure Flask is installed with version 2.1.0
+  provider => 'pip3',   # Use pip3 as the provider
+}
